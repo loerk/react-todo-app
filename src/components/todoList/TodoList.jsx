7cf9fa441d6cea57e4effe44f04003bad1cc05d2
loerk <fra.se@posteo.net>
2022-04-26 10:08:52 +0200

    first commit

---
+++ src/components/todoList/TodoList.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./TodoList.css";
+
+function TodoList({ todos, filter, deleteItem, setComplete }) {
+  return (
+    <div>
+      {todos
+
+        .sort((a, b) => a.timeInSeconds - b.timeInSeconds)
+        .filter((todo) => {
+          return (
+            filter === "all" ||
+            (filter === "open" && !todo.completed) ||
+            (filter === "done" && todo.completed)
+          );
+        })
+        .map((todo) => (
+          <li className="card" key={todo.id}>
+            <div className="cardText">
+              <p className={todo.completed ? "completed todoTime" : "todoTime"}>
+                {" "}
+                {todo.timestamp}
+              </p>
+              <p className={todo.completed ? "completed todoText" : "todoText"}>
+                {" "}
+                {todo.text}
+              </p>
+            </div>
+            <div className="cardButtonContainer">
+              <button
+                className="trashButton"
+                onClick={() => deleteItem(todo.id)}
+              >
+                <i className=" trash fa-solid fa-trash-can"></i>
+              </button>
+              <button
+                className="tickButton"
+                onClick={() => setComplete(todo.id)}
+              >
+                <i className="tick fa-solid fa-check"></i>
+              </button>
+            </div>
+          </li>
+        ))}
+    </div>
+  );
+}
+
+export default TodoList;
---
+++ src/container/App.js
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react";
+import TodoList from "../components/todoList/TodoList";
+import "./App.css";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos") || [])
+  );
+  const [filter, setFilter] = useState("all");
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        text: inputText,
+        completed: false,
+        id: Math.random() * 1000,
+        timestamp: new Date().toLocaleDateString("de-DE", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        }),
+        timeInSeconds: new Date().getMilliseconds(),
+      },
+    ]);
+    setInputText("");
+  };
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    setInputText(e.target.value);
+  };
+
+  const deleteItem = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+  const filterHandler = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const setComplete = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <>
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
+      <header>
+        <h1>Your Todolist</h1>
+      </header>
+      <div className="ball"></div>
+      <div className="statusBox">
+        <h2>You have {todos.length} items in this List</h2>
+        <h3>{todos.filter((todo) => todo.completed).length} completed</h3>
+        <h3>{todos.filter((todo) => !todo.completed).length} open</h3>
+      </div>
+      <div className="inputContainer">
+        <input onChange={handleInput} value={inputText}></input>
+        <button className="addButton" onClick={addTodo}>
+          <i className="fas fa-2x fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select
+            onChange={filterHandler}
+            name="todos"
+            className="filter-todos"
+          >
+            <option value="all">All</option>
+            <option value="done">Done</option>
+            <option value="open">Open</option>
+          </select>
+        </div>
+      </div>
+      <TodoList
+        todos={todos}
+        filter={filter}
+        deleteItem={deleteItem}
+        setComplete={setComplete}
+      />
+    </>
+  );
+}
+
+export default App;
